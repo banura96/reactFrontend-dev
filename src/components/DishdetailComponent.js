b1791ee3a0792a427b549bf019c7ea2d8ec6732e
banura96 <mabhettiarachchi@gmail.com>
2020-08-04 22:21:41 +0530

    add model for create comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,45 +1,23 @@
 import React, {Component}  from "react";
 import {Card, CardBody, CardTitle, CardText, CardImg,  
-  Button, Form, FormGroup,Input, Label} from "reactstrap";
+  Button, Row, Label, Modal, ModalBody, ModalHeader} from "reactstrap";
+import { Control, LocalForm, Errors} from 'react-redux-form';
 
-  const initialComment ={
-    name:'',
-    textarea:'',
-    rating:'1', 
-}
+
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+
 class Dishdetail extends Component{
-  // Modal, ModalBody, ModalHeader,
 constructor(props){
   super(props)
   this.state={
-    // isModelOpen:false,
-
-      name:'',
-      textarea:'',
-      rating:'1',
-      nameError:'',
-      initialState:initialComment,
-
-  
-    touched : {
-      name:false,
-      textarea:false
-    }
-
-   
+    
+    isModelOpen:false
   }
 
-
-  // this.name = React.createRef();
-  // this.rating = React.createRef();
-  // this.textarea = React.createRef();
-
-
   this.modalOpen = this.modalOpen.bind(this);
-  // this.handleLogin = this.handleLogin.bind(this);
-  this.handleBlur =this.handleBlur.bind(this);
-  this.handleSubmit=this.handleSubmit.bind(this);
-  // this.handleInputChange = this.handleInputChange.bind(this)
 }
 
 modalOpen(){
@@ -48,76 +26,14 @@ modalOpen(){
   })
 }
 
-// handleLogin(event) {
-//   this.modalOpen();
-//   if(event){
-//     alert("Username: " + this.username.value + " Password: " + this.password.value
-//     + " Remember: " + this.remember.checked);
-// event.preventDefault();
-//   }
-// }
 
-handleBlur =(field)=>(evt)=>{
-  this.setState({
-    touched:{...this.state.touched, [field]:true}
-  })
-}
-
-validate(){
-  // let nameError = '';
-  if(this.state.name.length < 3){
-    console.log("i run")
-   
-    this.setState({
-      nameError : 'Must be greater than 2 characters'
-    })
-    return false;
-  }
-  // if(this.nameError){
-  //   console.log("i am true")
- 
-  // }
-  // else if(this.state.touched.name && this.state.name.length>15){
-  //   errors.name = 'Must be 15 characters or less';
-  // }
-  return true;
-}
-
-handleInputChange=(event)=>{
-  // this.setState({
-  //   name : event.target.value,
-  //   rating:event.target.value,
-  //   textarea:event.target.value
-  // })
-  const target = event.target;
-  const value = target.name === 'isGoing' ? target.checked : target.value;
-  const name = target.name;
-
-  this.setState({
-    [name]: value
-  });
- 
-}
-
-
-handleSubmit=(event)=>{
-  event.preventDefault();
-    const isValid = this.validate();
-    if(isValid){
-      alert("Content is" +  this.state.name)
-      console.log(this.state.name.length)
-      this.setState({
-        initialState:initialComment
-      })
-      event.preventDefault();
-    }
-  
- 
-
+handleSubmit(values) {
+  console.log('Current State is: ' + JSON.stringify(values));
+  alert('Current State is: ' + JSON.stringify(values));
+  // event.preventDefault();
 }
 
   render(){
-    // const errors = this.validate(this.state.name)
     return (
       <div className="container">
         <div className="row">
@@ -161,73 +77,79 @@ handleSubmit=(event)=>{
               </CardBody>
               <div>
                 <Button outline color="secondary" onClick={this.modalOpen}>
-                  <i className="fa fa-pencil" aria-hidden="true"></i> Submit Comment
+                  <i className="fa fa-pencil" aria-hidden="true"></i> Submit
+                  Comment
                 </Button>
               </div>
             </Card>
           </div>
         </div>
-        {/* <Modal isOpen={this.state.isModelOpen} toggle={this.modalOpen}>
+        <Modal isOpen={this.state.isModelOpen} toggle={this.modalOpen}>
           <ModalHeader>Submit Comment</ModalHeader>
-          <ModalBody> */}
-            <Form onSubmit={this.handleSubmit}>
-              <FormGroup>
-                <Label htmlFor="rating">Rating</Label>
-                <Input
-                  type="select"
-                  id="rating"
-                  name="rating"
-                  // onBlur={this.handleBlur('rating')}
-                  onChange={this.handleInputChange}
-                  // innerRef={(input) => this.rating = input}
-                  value={this.state.rating}
-                  // ref={this.rating}
-                >
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
+          <ModalBody>
+            <div className="container">
+              <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                <Row className="form-group">
+                  <Label htmlFor="rating">Rating</Label>
 
-                </Input>
-              </FormGroup>
-              <FormGroup>
-                <Label htmlFor="name">Your Name</Label>
-                <Input  
-                  type="text"
-                  id="name"
-                  name="name"
-                  placeholder="Your Name"
-                  // valid={errors.name === ''}
-                  // invalid={errors.name !== ''}
-                  onBlur={this.handleBlur('name')}
-                  onChange={this.handleInputChange}
-                  // innerRef={(input) => this.name = input}
-                  value={this.state.name}
-                  // ref={this.name}
-                  // onBlur={this.handleBlur('name')}
-                />
-                <div style={{color:"red"}}>{this.state.nameError}</div>
-              </FormGroup>  
-              <FormGroup>
-                <Label htmlFor="textarea">Comment</Label>
-                <Input type="textarea"
-                name="textarea" 
-                id="textarea" 
-                rows="6" 
-                value={this.state.textarea}
-                // innerRef={(input) => this.textarea = input}
-                // onBlur={this.handleBlur('textarea')}
-                // ref={this.textarea}                  
-                onChange={this.handleInputChange}
+                  <Control.select
+                    model=".rating"
+                    name="rating"
+                    className="form-control"
+                  >
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </Control.select>
+                </Row>
+                <Row className="form-group">
+                  <Label htmlFor="firstname">Your Name</Label>
+
+                  <Control.text
+                    model=".firstname"
+                    id="firstname"
+                    name="firstname"
+                    placeholder="First Name"
+                    className="form-control"
+                    validators={{
+                      required,
+                      minLength: minLength(3),
+                      maxLength: maxLength(15),
+                    }}
                   />
-              </FormGroup>
-              <Button type="submit" value="submit" color="primary">
-                Submit
-              </Button>
-            </Form>
-          {/* </ModalBody>
-        </Modal> */}
+                  <Errors
+                    className="text-danger"
+                    model=".firstname"
+                    show="touched"
+                    messages={{
+                      required: "Required",
+                      minLength: "Must be greater than 2 characters",
+                      maxLength: "Must be 15 characters or less",
+                    }}
+                  />
+                </Row>
+                <Row className="form-group">
+                  <Label htmlFor="comment">Comment</Label>
+
+                  <Control.textarea
+                    model=".comment"
+                    id="comment"
+                    name="comment"
+                    rows="6"
+                    className="form-control"
+                  />
+                </Row>
+                <Row className="form-group">
+                  <Button type="submit" color="primary">
+                    Submit
+                  </Button>
+                </Row>
+              </LocalForm>
+            </div>
+          </ModalBody>
+        </Modal>
       </div>
     );
   }
